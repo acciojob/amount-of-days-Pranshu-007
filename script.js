c7a12034682074ee83ce36afa87721372a2586cb
Pranshu-007 <84780774+Pranshu-007@users.noreply.github.com>
2025-02-25 12:25:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const daysOfAYear = (year) => {
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return 366; 
+            } else {
+                return 365; 
+            }
+        } else {
+            return 366; 
+        }
+    } else {
+        return 365; 
+    }
+};
